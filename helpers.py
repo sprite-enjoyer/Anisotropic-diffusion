b28cd81116470bac3e00f6dcf55643bf82c729d3
sprite-enjoyer <samkharadze.saba01@gmail.com>
2023-12-26 20:15:53 +0400

    The repo is now fully documented

--- helpers.py
+++ helpers.py
@@ -29,6 +29,21 @@ def add_spn(image, salt_prob=0.02, pepper_prob=0.02):
     return noisy_image
 
 def get_images_from_path(path, return_filenames=False):
+  """
+    Load grayscale images from a folder.
+
+    Parameters:
+    - path (str): Path to the folder containing images.
+    - return_filenames (bool): If True, return a tuple with images and corresponding filenames. Default is False.
+
+    Returns:
+    - List of loaded images or a tuple (images, filenames) if return_filenames is True.
+
+    Example:
+    ```python
+    images = get_images_from_path("base_images")
+    ```
+    """
   images = []
 
   for filename in os.listdir(path):
@@ -41,11 +56,44 @@ def get_images_from_path(path, return_filenames=False):
   
   return images
 
+
 def convert_to_grayscale(source_path, save_path):
+  """
+    Convert color images to grayscale and save them.
+
+    Parameters:
+    - source_path (str): Path to the folder containing color images.
+    - save_path (str): Path to the folder where grayscale images will be saved.
+
+    Returns:
+    None
+
+    Example:
+    ```python
+    convert_to_grayscale("color_images", "grayscale_images")
+    ```
+    """
   images, filenames = get_images_from_path(source_path, return_filenames=True)
   save_images(images, save_path, filenames)
 
+
 def save_images(images, folder_path, names=[]):
+  """
+    Save a list of images to a folder with specified filenames.
+
+    Parameters:
+    - images (list): List of images to be saved.
+    - folder_path (str): Path to the folder where images will be saved.
+    - names (list): List of filenames corresponding to the images. Default is an empty list.
+
+    Returns:
+    None
+
+    Example:
+    ```python
+    save_images(images, "output_folder", ["image1.jpg", "image2.jpg"])
+    ```
+    """
   if len(names) != len(images): 
     raise ValueError("Length of the images must correspond to the length of the names")
   
@@ -53,7 +101,30 @@ def save_images(images, folder_path, names=[]):
     integer_image = np.uint8(images[i])
     Image.fromarray(integer_image).save(folder_path + "/" + names[i])
 
-def generate_noisy_images(source_path, save_path, salt_min=0.01, salt_max=0.2, pepper_min=0.01, pepper_max=0.2):
+
+def generate_noisy_images(source_path, save_path, salt_min=0.01, salt_max=0.3, pepper_min=0.01, pepper_max=0.3):
+  """
+  Generate noisy images by adding salt-and-pepper noise to grayscale images.
+
+  Parameters:
+  - source_path (str): Path to the folder containing the base grayscale images.
+  - save_path (str): Path to the folder where the noisy images will be saved.
+  - salt_min (float): Minimum salt noise probability. Default is 0.01.
+  - salt_max (float): Maximum salt noise probability. Default is 0.3.
+  - pepper_min (float): Minimum pepper noise probability. Default is 0.01.
+  - pepper_max (float): Maximum pepper noise probability. Default is 0.3.
+
+  Returns:
+  None
+
+  This function reads grayscale images from the source folder, applies salt-and-pepper noise
+  to each image multiple times, and saves the resulting noisy images to the specified folder.
+
+  Example:
+  ```python
+  generate_noisy_images("base_images", "noisy_images", salt_min=0.02, salt_max=0.2, pepper_min=0.01, pepper_max=0.25)
+  ```
+  """
   images = get_images_from_path(source_path)
   noisy_images = []
   salts = []
--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def exec(base_path, grayscale_path, noisy_path, result_path, noisy_generated=Tru
     generate_noisy_images(grayscale_path, noisy_path)
 
   images, file_names = get_images_from_path(noisy_path, return_filenames=True)
-  diffused_images = [anisodiff(image, kappa=80, num_iterations=2, methodNum=1) for image in images]
+  diffused_images = [anisodiff(image, kappa=100, num_iterations=1, methodNum=1) for image in images]
   save_images(diffused_images, result_path, file_names)
 
 exec(
